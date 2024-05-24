3db55630888361955209e19b78b3afe72479cb88
Daeyun <sweet_ray@naver.com>
2024-05-24 17:01:35 +0900

    Initial Commit

---
+++ TravelMagazine/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TravelMagazine
+//
+//  Created by 권대윤 on 5/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
