437372a43dfb1624317bceae8970a3ead4a1bd88
Daeyun <sweet_ray@naver.com>
2024-05-29 17:06:14 +0900

    Feat: 인기 도시 탭 생성

---
+++ TravelMagazine/Controller/PopularCity/PopularCityViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PopularCityViewController.swift
+//  TravelMagazine
+//
+//  Created by 권대윤 on 5/29/24.
+//
+
+import UIKit
+
+class PopularCityViewController: UIViewController {
+    
+    @IBOutlet var searchBar: UISearchBar!
+    @IBOutlet var segment: UISegmentedControl!
+    @IBOutlet var tableView: UITableView!
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+
+}
